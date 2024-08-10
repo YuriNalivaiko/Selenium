cd42e384f1916842b8837a0d9f9a01668d1d65b2
“Yuri <nalivaikoyuri@gmail.com>
2024-08-10 11:38:58 +0300

    initial commit

--- src/test/java/ru/netology/card/CallBackTest.java
+++ src/test/java/ru/netology/card/CallBackTest.java
@@ -21,7 +21,6 @@ public class CallBackTest {
     @BeforeEach
     public void setUp() {
 
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
